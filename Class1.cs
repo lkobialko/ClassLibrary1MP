3f99cec6dcf30b9128691d054376151cf2a22569
Liudmyla_Kobialko <Liudmyla_Kobialko@EPUAVINW0228>
2023-10-11 18:38:52 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1MP
+{
+    public class HelloWorld
+    {
+        public static string Main()
+        {
+            string currentTime = DateTime.Now.ToString("hh:mm:ss tt");
+            string userName = "Liudmyla";
+            return $"{currentTime} Hello, {userName}!";
+        }
+        }
+    }
